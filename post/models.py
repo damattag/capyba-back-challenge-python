13acd3c626dac2aa65b6284460ecb7061ee33cdd
damattag <guilherme.damatta@yahoo.com.br>
2023-11-15 19:05:54 -0300

    created models

--- post/models.py
+++ post/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField()
+    email = models.TextField(unique=True)
+    password = models.TextField()
+    image = models.TextField()
+    accepted_terms = models.BooleanField()
+    email_verified = models.BooleanField(default=False)
+    email_verification_token = models.TextField()
+    email_verification_token_expiry = models.DateTimeField()
+    role = models.TextField(default='user')
+
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.TextField()
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
+
+    author = models.ForeignKey(
+        'User', on_delete=models.CASCADE, related_name='posts')
+
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+    content = models.TextField()
+
+    author = models.ForeignKey(
+        'User', on_delete=models.CASCADE, related_name='comments')
+
+    post = models.ForeignKey(
+        'Post', on_delete=models.CASCADE, related_name='comments')
+
+    parent = models.ForeignKey(
+        'self', on_delete=models.CASCADE, related_name='children', null=True)
