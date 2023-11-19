5624308f2a3e100bcd02c25745ea36d9dd8b153b
damattag <guilherme.damatta@yahoo.com.br>
2023-11-18 21:01:08 -0300

    added filtering and sortering

--- posts/api/viewsets.py
+++ posts/api/viewsets.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
 from posts.api import serializers
 from posts import models
 
@@ -6,6 +7,12 @@ from posts import models
 class PostsViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.PostsSerializer
     queryset = models.Post.objects.all()
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['published']
+    search_fields = ['title', 'content']
+    ordering_fields = '__all__'
+    ordering = ['created_at']
 
 
 class UsersViewSet(viewsets.ModelViewSet):
@@ -16,3 +23,9 @@ class UsersViewSet(viewsets.ModelViewSet):
 class CommentsViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CommentsSerializer
     queryset = models.Comment.objects.all()
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter, filters.OrderingFilter]
+    filterset_fields = ['edited']
+    search_fields = ['content']
+    ordering_fields = '__all__'
+    ordering = ['created_at']
